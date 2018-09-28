49517205e910012ba1b72fab68108576a357c3d8
cmittal22 <42975501+cmittal22@users.noreply.github.com>
2018-09-28 12:27:33 -0400

    Add files via upload

---
+++ src/OperationTest.java
@@ -0,0 +1,21 @@
+
+public class OperationTest {
+
+	public static void main(String[] args)
+	{
+
+		int number; //Declaration statement
+		number = 12; //Assignment statement -> intialization 
+		number = number + 5;
+		System.out.println(number);
+		number/=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+	}
+
+}
